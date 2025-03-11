596c9ad4868834fdc3442773737ea1508d90c06d
Ceme4k1n <niger60.60@bk.ru>
2025-03-11 14:45:36 +0300

    Add: Validate user(initData)

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
 import express from 'express'
+import fs from 'fs'
+import https from 'https'
 import authRouter from './routes/auth'
+import path from 'path'
+
 const app = express()
 const PORT = process.env.PORT || '4000'
 
-const path = require('path')
-app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.json())
-
+app.use(express.static(path.join(__dirname, '../public')))
 app.use('/auth', authRouter)
+
+// const SSL_CERT_PATH = '/web/serf/certificate.crt'
+// const SSL_KEY_PATH = '/web/serf/certificate.key'
+// const SSL_CA_PATH = '/web/serf/certificate_ca.crt'
+
+// // Читаем сертификаты
+// const privateKey = fs.readFileSync(SSL_KEY_PATH, 'utf8')
+// const certificate = fs.readFileSync(SSL_CERT_PATH, 'utf8')
+// const ca = fs.readFileSync(SSL_CA_PATH, 'utf8')
+
+// const credentials = { key: privateKey, cert: certificate, ca: ca }
+
+// Запускаем HTTPS сервер
+// https.createServer(credentials, app).listen(PORT, () => {
+//   console.log(`Server is running on https://orchidshop.shop`)
+// })
 
 app.listen(PORT, () => {
   console.log(`⚡ Server is running at http://localhost:${PORT}`)
--- src/controllers/authControlles.ts
+++ src/controllers/authControlles.ts
@@ -1,16 +1,59 @@
 import { Request, Response } from 'express'
+import crypto from 'crypto'
+
 const dotenv = require('dotenv')
 dotenv.config()
 
-const TG_TOKEN = process.env.TG_TOKEN || 'test'
+const BOT_TOKEN = process.env.TG_TOKEN || 'test'
+const MAX_TIME_DIFF = 300 // 5 минут
 
 export const validate_user = (req: Request, res: Response) => {
-  const initData = req.body.initData
+  const initData = req.body?.initData
+  console.log(initData)
 
   if (!initData) {
-    res.status(403).json({ error: 'No initData' })
-    console.log('No initData')
-  } else {
-    console.log('Goyda')
+    res.status(403).json({ error: 'initData is required' })
+    return
   }
+
+  const validationResult = validateInitData(initData)
+
+  if (validationResult.error) {
+    res.status(validationResult.status).json({ error: validationResult.error })
+  }
+
+  // Если данные прошли валидацию
+  res.json({ success: true, message: 'User validated' })
+}
+
+// Функция для валидации данных initData
+export const validateInitData = (initData: string) => {
+  const params = new URLSearchParams(initData)
+  const hash = params.get('hash')
+  if (!hash) {
+    return { error: 'No hash provided', status: 400 }
+  }
+
+  params.delete('hash')
+
+  const dataCheckString = Array.from(params.entries())
+    .map(([key, value]) => `${key}=${value}`)
+    .sort()
+    .join('\n')
+
+  const secretKey = crypto.createHmac('sha256', 'WebAppData').update(BOT_TOKEN).digest()
+
+  const generatedHash = crypto.createHmac('sha256', secretKey).update(dataCheckString).digest('hex')
+
+  if (generatedHash !== hash) {
+    return { error: 'Invalid hash, data might be forged', status: 403 }
+  }
+
+  const authDate = parseInt(params.get('auth_date') || '0', 10)
+  const currentTime = Math.floor(Date.now() / 1000)
+
+  if (currentTime - authDate > MAX_TIME_DIFF) {
+    return { error: 'Data is too old', status: 403 }
+  }
+  return { success: true }
 }
