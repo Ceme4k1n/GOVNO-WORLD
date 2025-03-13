61e8c28d3758d6539d39121fab9f505329eab80f
saidanyx <perelad14@gmail.com>
2025-03-13 16:21:41 +0300

    css

--- src/routes/quiz.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { quiz_answers } from '../controllers/quizController'
-
-const router = express.Router()
-
-router.post('/submit', quiz_answers)
-
-export default router
