ebe5255657e382e8c0ff5ed1e957051921ccd924
saidanyx <perelad14@gmail.com>
2025-03-27 09:25:38 +0300

    адаптация для профиля, рефералов, боковой панели и header

--- js/lets.js
+++ js/lets.js
@@ -8,38 +8,38 @@ function updateFontSize() {
     document.documentElement.style.setProperty('--font-verybig', '2.3vw');
 
     if (width <= 1440) {
-        document.documentElement.style.setProperty('--font-small', '1vw');
+        document.documentElement.style.setProperty('--font-small', '1.2vw');
         document.documentElement.style.setProperty('--font-normal', '1.4vw');
         document.documentElement.style.setProperty('--font-big', '2.2vw');
         document.documentElement.style.setProperty('--font-verybig', '3vw');
     }
 
     if (width <= 1024) {
-        document.documentElement.style.setProperty('--font-small', '1.2vw');
+        document.documentElement.style.setProperty('--font-small', '1.6vw');
         document.documentElement.style.setProperty('--font-normal', '1.8vw');
         document.documentElement.style.setProperty('--font-big', '2.7vw');
         document.documentElement.style.setProperty('--font-verybig', '4vw');
     }
 
     if (width <= 768) {
-        document.documentElement.style.setProperty('--font-small', '2vw');
+        document.documentElement.style.setProperty('--font-small', '2.2vw');
         document.documentElement.style.setProperty('--font-normal', '2.5vw');
         document.documentElement.style.setProperty('--font-big', '4vw');
         document.documentElement.style.setProperty('--font-verybig', '5.5vw');
     }
 
     if (width <= 600) {
-        document.documentElement.style.setProperty('--font-small', '2.5vw');
+        document.documentElement.style.setProperty('--font-small', '3.2vw');
         document.documentElement.style.setProperty('--font-normal', '3.8vw');
         document.documentElement.style.setProperty('--font-big', '5.2vw');
         document.documentElement.style.setProperty('--font-verybig', '8vw');
     }
 
     if (width <= 480) {
-        document.documentElement.style.setProperty('--font-small', '3vw');
+        document.documentElement.style.setProperty('--font-small', '4vw');
         document.documentElement.style.setProperty('--font-normal', '4.8vw');
-        document.documentElement.style.setProperty('--font-big', '7vw');
-        document.documentElement.style.setProperty('--font-verybig', '10vw');
+        document.documentElement.style.setProperty('--font-big', '6vw');
+        document.documentElement.style.setProperty('--font-verybig', '9vw');
     }
 }
 
